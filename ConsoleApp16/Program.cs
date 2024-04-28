acb7f6bae3e95fc18dca80b4d0cd18e966992719
Maksonch1k <123887520+Maksonch1k@users.noreply.github.com>
2024-04-28 19:46:16 +0300

    Add files via upload

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  int
+            Triangle<int> intTriangle = new Triangle<int>(3, 4, 5);
+            Console.WriteLine("Triangle with sides of type int:");
+            intTriangle.DisplayInfo();
+            Console.WriteLine($"Area: {intTriangle.CalculateArea()}");
+
+            //  double
+            Triangle<double> doubleTriangle = new Triangle<double>(5.5, 6.5, 7.5);
+            Console.WriteLine("\nTriangle with sides of type double:");
+            doubleTriangle.DisplayInfo();
+            Console.WriteLine($"Area: {doubleTriangle.CalculateArea()}");
+
+            // uint
+            Triangle<uint> uintTriangle = new Triangle<uint>(2, 3, 4);
+            Console.WriteLine("\nTriangle with sides of type uint:");
+            uintTriangle.DisplayInfo();
+            Console.WriteLine($"Area: {uintTriangle.CalculateArea()}");
+        }
+    }
+    }
+
---
+++ ConsoleApp16/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Triangle<T>
+    {
+        private T sideA;
+        private T sideB;
+        private T sideC;
+
+        public Triangle(T a, T b, T c)
+        {
+            sideA = a;
+            sideB = b;
+            sideC = c;
+        }
+
+        public T SideA
+        {
+            get { return sideA; }
+            set { sideA = value; }
+        }
+
+        public T SideB
+        {
+            get { return sideB; }
+            set { sideB = value; }
+        }
+
+        public T SideC
+        {
+            get { return sideC; }
+            set { sideC = value; }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Side A: {sideA}, Side B: {sideB}, Side C: {sideC}");
+        }
+
+        public double CalculateArea()
+        {
+            double a = Convert.ToDouble(sideA);
+            double b = Convert.ToDouble(sideB);
+            double c = Convert.ToDouble(sideC);
+
+            double semiPerimeter = (a + b + c) / 2;
+            double area = Math.Sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
+
+            return area;
+        }
+    }
+}
